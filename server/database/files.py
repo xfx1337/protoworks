fae14d955e4b60a38849a307e9d6db30a0b356f4
xfx1337 <54548160+xfx1337@users.noreply.github.com>
2024-03-09 10:49:01 +0500

    db trying to fix

--- server/database/files.py
+++ server/database/files.py
@@ -17,16 +17,16 @@ class Files:
         """)
     
     def register_update(self, data):
-        write_sql = "REPLACE INTO files (path, date_modified, file_size) VALUES "
+        write_sql = "INSERT INTO files (path, date_modified, file_size) VALUES "
         for f in data:
             path = f["path"]
             date = int(f["date_modified"])
             file_size = int(f["size"])
-            row = f"({path}, {date}, {file_size}),"
+            row = f"('{path}', {date}, {file_size}),"
             write_sql += row
-        write_sql = write_sql[:-2]
-        write_sql += ";"
-
+        write_sql = write_sql[:-1]
+        write_sql += "\nON DUPLICATE KEY UPDATE date_modified=VALUES(date_modified), file_size=VALUES(file_size)"
+        breakpoint()
         self.cursor.execute(write_sql)
         self.connection.commit()
 
